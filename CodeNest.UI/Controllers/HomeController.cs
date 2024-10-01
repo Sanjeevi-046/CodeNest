d9a65d17656f129198d7132d2d16162f678dceb2
Sanjeevi Venkadachalapathi <svenkadachalapathi@ctg.com>
2024-10-01 12:16:13 +0530

    New_Version - Side menu Changes and login page  setup

--- CodeNest.UI/Controllers/HomeController.cs
+++ CodeNest.UI/Controllers/HomeController.cs
@@ -32,9 +32,5 @@ public class HomeController : Controller
     {
         return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
      
-        
-        
-       
-
     }
 }
