0cbe73f840385000de7a884fb16aa6eeb27f5e29
Sanjeevi Venkadachalapathi <svenkadachalapathi@ctg.com>
2024-10-03 12:15:23 +0530

    Naming changes, Modal changes and Impelemented userRepository

---
+++ CodeNest.DAL/Models/CustomHtml.cs
@@ -0,0 +1,36 @@
+﻿// ***********************************************************************************************
+//
+//  (c) Copyright 2023, Computer Task Group, Inc. (CTG)
+//
+//  This software is licensed under a commercial license agreement. For the full copyright and
+//  license information, please contact CTG for more information.
+//
+//  Description: Sample Description.
+//
+// ***********************************************************************************************
+
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace CodeNest.DAL.Models
+{
+    public class CustomHtml
+    {
+        [BsonId]
+        [BsonElement("_id"), BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; } = string.Empty;
+
+        public string HtmlInput { get; set; }
+        public string HtmlOutput { get; set; }
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string CreatedBy { get; set; }
+        public string CreatedOn { get; set; }
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string? ModifiedBy { get; set; }
+        public string? ModifiedOn { get; set; }
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Workspaces { get; set; }
+
+        public string Version { get; set; }
+    }
+}
