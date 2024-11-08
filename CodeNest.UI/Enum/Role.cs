8b55e307369c152bf30c561d61a835d3ec4e8fa3
Sanjeevi Venkadachalapathi <svenkadachalapathi@ctg.com>
2024-11-08 14:00:42 +0530

    Implemented the Cookie authentication

---
+++ CodeNest.UI/Enum/Role.cs
@@ -0,0 +1,19 @@
+﻿// ***********************************************************************************************
+//
+//  (c) Copyright 2024, Computer Task Group, Inc. (CTG)
+//
+//  This software is licensed under a commercial license agreement. For the full copyright and
+//  license information, please contact CTG for more information.
+//
+//  Description: CodeNest .
+//
+// ***********************************************************************************************
+
+namespace CodeNest.UI.Enum
+{
+    public enum Role
+    {
+        Admin = 1,
+        User = 2
+    }
+}
